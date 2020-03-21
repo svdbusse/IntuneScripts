530e4ebd4d075f25fa391a5258601337608c298d
svdbusse <Svdbusse@gmail.com>
2020-03-21 20:24:38 +1300

    Update Set-PrimaryUserfromLastLogIn.ps1
    Changed script to:
    
    Get all Windows 10 Devices from the Tenant
    Foreach device, checks who is the current Primary User of the Device
    Checks that the last user that logged in from the usersLoggedOn attribute is not the Primary User of the Device
    If True: Sets the last logged on user to the Primary User of the device
    If False: Moves onto the next device

--- PrimaryUser/Set-PrimaryUserfromLastLogIn.ps1
+++ PrimaryUser/Set-PrimaryUserfromLastLogIn.ps1
@@ -443,69 +443,53 @@ else {
 
 ####################################################
 
-if(!$DeviceName){
+#Get All Windows 10 Intune Managed Devices for the Tenant
+$Devices = Get-Win10IntuneManagedDevice
 
-    Write-Host
-    write-host "Intune Device Name:" -f Yellow
-    $DeviceName = Read-Host
+Foreach ($Device in $Devices){ 
 
-}
+        Write-Host "Device name:" $device."deviceName" -ForegroundColor Cyan
+        $IntuneDevicePrimaryUser = Get-IntuneDevicePrimaryUser -deviceId $Device.id
 
-if(!$UserPrincipalName){
+        #Check if there is a Primary user set on the device already
+        if($IntuneDevicePrimaryUser -eq $null){
 
-    Write-Host
-    write-host "User Principal Name:" -f Yellow
-    $UserPrincipalName = Read-Host
-
-}
-
-$Device = Get-Win10IntuneManagedDevice -deviceName "$DeviceName"
-
-if($Device){
-
-    Write-Host "Device name:" $device."deviceName" -ForegroundColor Cyan
-    $IntuneDevicePrimaryUser = Get-IntuneDevicePrimaryUser -deviceId $Device.id
-
-    if($IntuneDevicePrimaryUser -eq $null){
-
-        Write-Host "No Intune Primary User Id set for Intune Managed Device" $Device."deviceName" -f Red 
-
-    }
-
-    else {
-    
-        Write-Host "Intune Device Primary User:" $IntuneDevicePrimaryUser
-
-    }
-
-    $User = Get-AADUser -userPrincipalName $UserPrincipalName
-
-    $AADUserName = $User.displayName
-    
-        if($IntuneDevicePrimaryUser -notmatch $User.id){
-
-            $SetIntuneDevicePrimaryUser = Set-IntuneDevicePrimaryUser -IntuneDeviceId $Device.id -userId $User.id
-
-            if($SetIntuneDevicePrimaryUser -eq ""){
-
-                Write-Host "User"$User.displayName"set as Primary User for device '$DeviceName'..." -ForegroundColor Green
-
-            }
+            Write-Host "No Intune Primary User Id set for Intune Managed Device" $Device."deviceName" -f Red 
 
         }
 
         else {
-
-            Write-Host "The user '$AADUserName' specified is already the Primary User on the device..." -ForegroundColor Red
+            $PrimaryAADUser = Get-AADUser -userPrincipalName $IntuneDevicePrimaryUser
+            Write-Host "Intune Device Primary User:" $PrimaryAADUser.displayName
 
         }
 
+        #Get the objectID of the last logged in user for the device, which is the last object in the list of usersLoggedOn
+        $LastLoggedInUser = ($Device.usersLoggedOn[-1]).userId
+
+        #Using the objectID, get the user from the Microsoft Graph for logging purposes
+        $User = Get-AADUser -userPrincipalName $LastLoggedInUser
+    
+            #Check if the current primary user of the device is the same as the last logged in user
+            if($IntuneDevicePrimaryUser -notmatch $User.id){
+
+                #If the user does not match, then set the last logged in user as the new Primary User
+                $SetIntuneDevicePrimaryUser = Set-IntuneDevicePrimaryUser -IntuneDeviceId $Device.id -userId $User.id
+
+                if($SetIntuneDevicePrimaryUser -eq ""){
+
+                    Write-Host "User"$User.displayName"set as Primary User for device '$($Device.deviceName)'..." -ForegroundColor Green
+
+                }
+
+            }
+
+            else {
+                #If the user is the same, then write to host that the primary user is already correct.
+                Write-Host "The user '$($User.displayName)' is already the Primary User on the device..." -ForegroundColor Yellow
+
+            }
+
+    Write-Host
+
 }
-
-else {
-
-    Write-Host "Intune Device '$DeviceName' can't be found..." -ForegroundColor Red
-
-}
-
-Write-Host
